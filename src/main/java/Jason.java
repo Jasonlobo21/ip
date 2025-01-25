0705a2c7cba82a468abc5188310f726d80c04b55
jasonlobo21 <jasonlobo2121@gmail.com>
2025-01-26 00:40:11 +0800

    Jason's greetings

---
+++ src/main/java/Jason.java
@@ -0,0 +1,7 @@
+public class Jason {
+    public static void main(String[] args) {
+        System.out.println(" Hello! I'm Jason\n" +
+                " What can I do for you?\n");
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
